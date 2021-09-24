809e36f91f5c82c67d04972b1e6c0ff4095ad50f
will <56336577+will0999@users.noreply.github.com>
2021-09-24 16:45:14 -0300

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-from seed import loadComputers, loadResources
-
-if __name__ == '__main__':
-    computadores = loadComputers()
-    print(computadores)
-    recursos = loadResources()
-    print(recursos)
